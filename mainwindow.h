2daa1ba8d82ebbf6ee1df9ef7532c393f8aeea59
SegiJo <jsg2419@gmail.com>
2024-11-14 17:08:49 +0900

    [2024-11-14] feat : Logo add
    Logo add and layout modify

--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,13 @@
 #include <QImage>
 #include "video_thread.h"  // VideoThread 클래스 헤더
 
+#include <QStackedWidget>
+#include <QPushButton>
+#include <QtCharts/QChartView>
+#include <QScrollArea>
+
+QT_CHARTS_USE_NAMESPACE
+
 namespace Ui {
 class MainWindow;
 }
@@ -20,9 +27,16 @@ public:
 private slots:
     void updateFrame(const QImage &frame);
 
+    void showChartPage();
+    void showImagePage();
+
 private:
     Ui::MainWindow *ui;
     VideoThread *videoThread;
+
+    QStackedWidget *stackedWidget;
+    QChartView *chartView;
+    QScrollArea *imageScrollArea;
 };
 
 #endif // MAINWINDOW_H
